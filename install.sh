a873686b6b029dd962abfd4014c2f04d7aa16da0
Jose de Jesus Chavez <chavezjal06@hotmail.com>
2021-01-22 19:15:43 -0600

    Modified install script

--- install.sh
+++ install.sh
@@ -1,4 +1,8 @@
-echo "nameserver 8.8.8.8" | sudo tee /etc/resolv.conf > /dev/null
+#!/bin/bash
+
+echo "Todo bien! Hasta luego"
+
+#echo "nameserver 8.8.8.8" | sudo tee /etc/resolv.conf > /dev/null
 sudo apt update
 sudo apt install python3-pip
 pip3 install dotbot
