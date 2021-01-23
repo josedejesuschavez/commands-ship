99c32d51359426cbc892d43e85e534954b2bc512
José Chávez <chavezjal06@hotmail.com>
2021-01-22 23:17:00 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,6 +7,6 @@ sudo apt update
 sudo apt install python3-pip -y
 pip3 install dotbot
 sudo apt install zsh -y
-#chsh -s $(which zsh)
+chsh -s $(which zsh)
 
 #sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
