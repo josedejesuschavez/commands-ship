36bab1eab764915ab64ff800ca70f6fb70fabe58
José Chávez <chavezjal06@hotmail.com>
2021-01-22 23:03:19 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ echo "---Installing configs ---"
 #echo "nameserver 8.8.8.8" | sudo tee /etc/resolv.conf > /dev/null
 sudo apt update
 sudo apt install python3-pip -y
-#pip3 install dotbot
+pip3 install dotbot
 sudo apt install zsh -y
 #chsh -s $(which zsh)
 
