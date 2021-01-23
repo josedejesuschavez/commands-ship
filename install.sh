5e10dd30e170b9e9d83ef7171cb96bda4db5119e
Jose de Jesus Chavez <chavezjal06@hotmail.com>
2021-01-22 20:08:13 -0600

    Modify for autoinstall pip

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-echo "Todo bien! Hasta luego"
+echo "---Installing configs---"
 
 #echo "nameserver 8.8.8.8" | sudo tee /etc/resolv.conf > /dev/null
 sudo apt update
-sudo apt install python3-pip
+sudo apt install python3-pip -y
 pip3 install dotbot
 dotbot -c symlinks/conf.yaml
