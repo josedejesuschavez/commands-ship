a28b89e60d109570f16b4a74c00f7f551cce5e72
José Chávez <chavezjal06@hotmail.com>
2021-01-23 00:27:43 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,9 +4,10 @@ echo "---Installing configs ---"
 
 #echo "nameserver 8.8.8.8" | sudo tee /etc/resolv.conf > /dev/null
 sudo apt update
-sudo apt install zsh -y
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 sudo apt install python3-pip -y
 pip3 install dotbot
 export PATH=/home/jchavez/.local/bin:$PATH
 dotbot -c symlinks/conf.yaml
+sudo apt install zsh -y
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
