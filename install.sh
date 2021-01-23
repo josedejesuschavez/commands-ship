56471c077fbd0d382152cfa09ba7ad0efb2f20bf
José Chávez <chavezjal06@hotmail.com>
2021-01-23 01:07:46 -0600

    Update install.sh
    Update for install fzf

--- install.sh
+++ install.sh
@@ -7,6 +7,7 @@ sudo apt update
 sudo apt install python3-pip -y
 pip3 install dotbot
 /home/jchavez/.local/bin/dotbot -c symlinks/conf.yaml
+sudo apt install fzf
 sudo apt install zsh -y
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
