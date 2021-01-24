97ddae335d42fb083215d66cd1f68360fb6f373e
José Chávez <chavezjal06@hotmail.com>
2021-01-24 00:28:26 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
 echo "---Installing configs ---"
-
-
-#echo "nameserver 8.8.8.8" | sudo tee /etc/resolv.conf > /dev/null
 sudo apt update
 sudo apt install python3-pip -y
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 pip3 install dotbot
 $HOME/.local/bin/dotbot -c $HOME/.dotfiles/symlinks/conf.yaml -d $HOME/.dotfiles
 sudo apt install fzf
