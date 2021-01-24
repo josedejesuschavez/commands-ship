abc7c0ff4f624c2a57a1b863927b4adbe5119c8a
Jose de Jesus Chavez <chavezjal06@hotmail.com>
2021-01-23 21:43:36 -0600

    Added export path for dotbot

--- shell/exports.sh
+++ shell/exports.sh
@@ -1,18 +1,20 @@
-export path=(
-  "$HOME/bin"
-  "$DOTLY_PATH/bin"
-  "$DOTFILES_PATH/bin"
-  "$JAVA_HOME/bin"
-  "$GEM_HOME/bin"
-  "$GOPATH/bin"
-  "$HOME/.cargo/bin"
-  "/usr/local/opt/ruby/bin"
-  "/usr/local/opt/python@3.9/libexec/bin"
-  "/usr/local/bin"  # This contains all Brew binaries (zsh...)
-  "/usr/local/sbin" # This contains all Brew binaries
-  "/bin"            # macOS zsh is here
-  "/usr/bin"
-  "/usr/sbin"
-  "/sbin"
-  "/home/jchavez/.local/bin"
-)
+#export path=(
+#  "$HOME/bin"
+#  "$DOTLY_PATH/bin"
+#  "$DOTFILES_PATH/bin"
+#  "$JAVA_HOME/bin"
+#  "$GEM_HOME/bin"
+#  "$GOPATH/bin"
+#  "$HOME/.cargo/bin"
+#  "/usr/local/opt/ruby/bin"
+#  "/usr/local/opt/python@3.9/libexec/bin"
+#  "/usr/local/bin"  # This contains all Brew binaries (zsh...)
+#  "/usr/local/sbin" # This contains all Brew binaries
+#  "/bin"            # macOS zsh is here
+#  "/usr/bin"
+#  "/usr/sbin"
+#  "/sbin"
+#  "/home/jchavez/.local/bin"
+#)
+
+export PATH=$HOME/.local/bin:$PATH
