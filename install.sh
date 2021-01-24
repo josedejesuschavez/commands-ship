906baf9fca60459029223c723f77b12b11ce8ec6
José Chávez <chavezjal06@hotmail.com>
2021-01-23 21:24:23 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,13 +7,9 @@ echo "---Installing configs ---"
 sudo apt update
 sudo apt install python3-pip -y
 pip3 install dotbot
-export DOTFILES_PATH="${HOME}/.dotfiles"
-$HOME/.local/bin/dotbot -c $HOME/.dotfiles/symlinks/conf.yaml
+$HOME/.local/bin/dotbot -c $HOME/.dotfiles/symlinks/conf.yaml -d $HOME/.dotfiles
 sudo apt install fzf
 sudo apt install zsh -y
-#sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-#curl -fsSL https://raw.githubusercontent.com/zimfw/install/master/install.zsh | zsh
-
 chsh -s $(which zsh)
 mkdir $HOME/.zim
 wget -O $HOME/.zim/zimfw.zsh https://github.com/zimfw/zimfw/releases/latest/download/zimfw.zsh
