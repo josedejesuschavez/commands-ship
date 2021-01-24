e96ea343f4caf287d8cc7ece1ec6b37845b6fdf8
José Chávez <chavezjal06@hotmail.com>
2021-01-24 02:01:14 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,12 +3,11 @@
 echo "---Installing configs ---"
 sudo apt update
 sudo apt install python3-pip -y
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 pip3 install dotbot
 $HOME/.local/bin/dotbot -c $HOME/.dotfiles/symlinks/conf.yaml -d $HOME/.dotfiles
-sudo apt install fzf
-sudo apt install zsh -y
+sudo apt install fzf zsh -y
 chsh -s $(which zsh)
 mkdir $HOME/.zim
 wget -O $HOME/.zim/zimfw.zsh https://github.com/zimfw/zimfw/releases/latest/download/zimfw.zsh
 zsh ~/.zim/zimfw.zsh install
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
