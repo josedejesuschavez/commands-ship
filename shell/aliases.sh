03514feb4a47a7f28a8744afc087c22da08aab09
Jose de Jesus Chavez <chavezjal06@hotmail.com>
2021-02-27 17:35:41 -0600

    Add alias for change to folder workspace

--- shell/aliases.sh
+++ shell/aliases.sh
@@ -3,11 +3,9 @@ alias sudo='sudo '
 
 alias ..="cd .."
 alias ...="cd ../.."
-alias ll="exa -l"
-alias la="exa -la"
 
 # Jumps
 alias ~="cd ~"
-alias tmp="cd ~/Desktop/tmp"
+alias workspace="/mnt/c/workspace"
 
 alias c.='code .'
