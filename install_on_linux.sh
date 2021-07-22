3e0b528377a6e274fd944153ddd06b924fb505f5
José Chávez <chavezjal06@hotmail.com>
2021-07-22 01:46:23 -0500

    Update install_on_linux.sh

--- install_on_linux.sh
+++ install_on_linux.sh
@@ -8,19 +8,22 @@ if ! [ -x "$(command -v brew)" ]; then
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
 
-${brew_path}/brew install wget
-${brew_path}/brew install python
-${brew_path}/brew install zsh
-${brew_path}/brew install fzf
-
-${brew_path}/pip3 install dotbot
-
-${brew_path}/dotbot -c $HOME/.commands-ship/configs/symlinks.yaml -d $HOME/.commands-ship
-
 export PATH="${brew_path}:$PATH"
 
+brew list -1 | xargs brew rm
+brew cleanup
+
+brew install wget
+brew install python
+brew install zsh
+brew install fzf
+
+pip3 install dotbot
+
+dotbot -c $HOME/.commands-ship/configs/symlinks.yaml -d $HOME/.commands-ship
+
 #chsh -s $(which zsh)
-sudo chsh -s ${brew_path}/zsh "${USER}"
+sudo chsh -s zsh "${USER}"
 
 if [ ! -d "$HOME/.zim" ]; then
   mkdir $HOME/.zim
