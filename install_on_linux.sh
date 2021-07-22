76cdec6d2f2a7cb01268c549f6043aed4761ced5
José Chávez <chavezjal06@hotmail.com>
2021-07-22 02:16:18 -0500

    Update install_on_linux.sh

--- install_on_linux.sh
+++ install_on_linux.sh
@@ -13,22 +13,22 @@ export PATH="${brew_path}:$PATH"
 #brew list -1 | xargs brew rm
 #brew cleanup
 
-brew install wget
-brew install python
-brew install zsh
-brew install fzf
+${brew_path}/brew install wget
+${brew_path}/brew install python
+${brew_path}/brew install zsh
+${brew_path}/brew install fzf
 
-pip3 install dotbot
+${brew_path}/pip3 install dotbot
 
-dotbot -c $HOME/.commands-ship/configs/symlinks.yaml -d $HOME/.commands-ship
+${brew_path}/dotbot -c $HOME/.commands-ship/configs/symlinks.yaml -d $HOME/.commands-ship
 
 #chsh -s $(which zsh)
-sudo chsh -s zsh "${USER}"
+sudo chsh -s ${brew_path}/zsh "${USER}"
 
 if [ ! -d "$HOME/.zim" ]; then
   mkdir $HOME/.zim
-  wget -O $HOME/.zim/zimfw.zsh https://github.com/zimfw/zimfw/releases/latest/download/zimfw.zsh
-  zsh ~/.zim/zimfw.zsh install
+  ${brew_path}/wget -O $HOME/.zim/zimfw.zsh https://github.com/zimfw/zimfw/releases/latest/download/zimfw.zsh
+  ${brew_path}/zsh ~/.zim/zimfw.zsh install
 fi
 
 
