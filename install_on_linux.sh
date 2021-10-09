47b56f88ab3f5c67e2d11f4463c1a6992b9df284
José Chávez <chavezjal06@hotmail.com>
2021-10-09 00:30:26 -0500

    Update install_on_linux.sh

--- install_on_linux.sh
+++ install_on_linux.sh
@@ -14,34 +14,26 @@ fi
 
 export PATH="${brew_path}:$PATH"
 
-brew list -1 | xargs brew rm
-brew cleanup
+#${brew_path}/brew list -1 | xargs ${brew_path}/brew rm
+#${brew_path}/brew cleanup
 
-brew install wget
-brew install python
-brew install zsh
-brew install fzf
-brew install git
+${brew_path}/brew install wget
+${brew_path}/brew install python
+${brew_path}/brew install zsh
+${brew_path}/brew install fzf
+${brew_path}/brew install git
 
-pip3 install dotbot
+${brew_path}/pip3 install dotbot
 
-echo "Installed dotbot"
+${brew_path}/dotbot -c $HOME/.commands-ship/configs/symlinks.yaml -d $HOME/.commands-ship
 
-#${brew_path}/dotbot -c $HOME/.commands-ship/configs/symlinks.yaml -d $HOME/.commands-ship
+sudo chsh -s ${brew_path}/zsh "${USER}"
 
-#chsh -s $(which zsh)
-#sudo chsh -s ${brew_path}/zsh "${USER}"
-
-#if [ ! -d "$HOME/.zim" ]; then
-#  mkdir $HOME/.zim
-#  ${brew_path}/wget -O $HOME/.zim/zimfw.zsh https://github.com/zimfw/zimfw/releases/latest/download/zimfw.zsh
-#  ${brew_path}/zsh ~/.zim/zimfw.zsh install
-#fi
-
-#sudo apt-get remove git
-#sudo apt-get -y purge python3.8
-#sudo apt-get -y autoremove
-
+if [ ! -d "$HOME/.zim" ]; then
+  mkdir $HOME/.zim
+  ${brew_path}/wget -O $HOME/.zim/zimfw.zsh https://github.com/zimfw/zimfw/releases/latest/download/zimfw.zsh
+  ${brew_path}/zsh ~/.zim/zimfw.zsh install
+fi
 
 #curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
 #	https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
