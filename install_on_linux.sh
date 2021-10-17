a909d6ed8db1ae898dd5866ac91cd4ad4cf470a8
José Chávez <chavezjal06@hotmail.com>
2021-10-16 19:48:29 -0500

    Update install_on_linux.sh

--- install_on_linux.sh
+++ install_on_linux.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
 sudo apt update
-
+sudo apt-get install build-essential procps curl file git
 if ! [ -x "$(command -v brew)" ]; then
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
 
 brew_path=$(find /home -type d | grep .linuxbrew/bin)
 
-export PATH="${brew_path}:$PATH"
+#export PATH="${brew_path}:$PATH"
 
-sudo apt-get remove git
-sudo apt-get -y purge python3.8
-sudo apt-get -y autoremove
+#sudo apt-get remove git
+#sudo apt-get -y purge python3.8
+#sudo apt-get -y autoremove
 
 #${brew_path}/brew list -1 | xargs ${brew_path}/brew rm
 #${brew_path}/brew cleanup
@@ -25,15 +25,15 @@ ${brew_path}/brew install git
 
 ${brew_path}/pip3 install dotbot
 
-${brew_path}/dotbot -c $HOME/.commands-ship/configs/symlinks.yaml -d $HOME/.commands-ship
+#${brew_path}/dotbot -c $HOME/.commands-ship/configs/symlinks.yaml -d $HOME/.commands-ship
 
-sudo chsh -s ${brew_path}/zsh "${USER}"
+#sudo chsh -s ${brew_path}/zsh "${USER}"
 
-if [ ! -d "$HOME/.zim" ]; then
-  mkdir $HOME/.zim
-  ${brew_path}/wget -O $HOME/.zim/zimfw.zsh https://github.com/zimfw/zimfw/releases/latest/download/zimfw.zsh
-  ${brew_path}/zsh ~/.zim/zimfw.zsh install
-fi
+#if [ ! -d "$HOME/.zim" ]; then
+#  mkdir $HOME/.zim
+#  ${brew_path}/wget -O $HOME/.zim/zimfw.zsh https://github.com/zimfw/zimfw/releases/latest/download/zimfw.zsh
+#  ${brew_path}/zsh ~/.zim/zimfw.zsh install
+#fi
 
 #curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
 #	https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
