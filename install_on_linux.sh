45ceebd1409bdc48b6168d151fbe602febf153e3
José Chávez <chavezjal06@hotmail.com>
2021-10-16 20:48:02 -0500

    Update install_on_linux.sh

--- install_on_linux.sh
+++ install_on_linux.sh
@@ -25,15 +25,15 @@ ${brew_path}/brew install git
 
 ${brew_path}/pip3 install dotbot
 
-#${brew_path}/dotbot -c $HOME/.commands-ship/configs/symlinks.yaml -d $HOME/.commands-ship
+${brew_path}/dotbot -c $HOME/.commands-ship/configs/symlinks.yaml -d $HOME/.commands-ship
 
-#sudo chsh -s ${brew_path}/zsh "${USER}"
+sudo chsh -s ${brew_path}/zsh "${USER}"
 
-#if [ ! -d "$HOME/.zim" ]; then
-#  mkdir $HOME/.zim
-#  ${brew_path}/wget -O $HOME/.zim/zimfw.zsh https://github.com/zimfw/zimfw/releases/latest/download/zimfw.zsh
-#  ${brew_path}/zsh ~/.zim/zimfw.zsh install
-#fi
+if [ ! -d "$HOME/.zim" ]; then
+  mkdir $HOME/.zim
+  ${brew_path}/wget -O $HOME/.zim/zimfw.zsh https://github.com/zimfw/zimfw/releases/latest/download/zimfw.zsh
+  ${brew_path}/zsh ~/.zim/zimfw.zsh install
+fi
 
 #curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
 #	https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
