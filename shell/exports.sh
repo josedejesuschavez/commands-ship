4415258e0754356a7474106a8e64731843c234b8
José Chávez <chavezjal06@hotmail.com>
2021-11-03 23:34:30 -0600

    Update exports.sh

--- shell/exports.sh
+++ shell/exports.sh
@@ -2,3 +2,4 @@ export PATH=$HOME/.local/bin:$PATH
 export PATH=/home/linuxbrew/.linuxbrew/bin:$PATH
 export PATH=/home/linuxbrew/.linuxbrew/sbin:$PATH
 export PATH=$DOTFILES_PATH/bin:$PATH
+export PATH=$PATH:/snap/bin
