9c67204fdb2a8f98b54997054071de7026002557
José Chávez <chavezjal06@hotmail.com>
2021-11-04 17:13:20 -0600

    Update init.sh

--- shell/init.sh
+++ shell/init.sh
@@ -4,14 +4,7 @@ source "$DOTFILES_PATH/shell/aliases.sh"
 source "$DOTFILES_PATH/shell/exports.sh"
 source "$DOTFILES_PATH/shell/functions.sh"
 
-#Update/script
-#UpdateCommandsFromGit/script
-
 if [ -f "$HOME/.zim/zimfw.zsh" ]; then
     zsh ~/.zim/zimfw.zsh install
     rm $HOME/.zim/zimfw.zsh
-    
-    curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-    vim +'PlugInstall --sync' +qa
 fi
