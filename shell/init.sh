ce559af69162b8ca3251b2fdb980b6202611f839
José Chávez <chavezjal06@hotmail.com>
2021-11-04 22:36:20 -0600

    Update init.sh

--- shell/init.sh
+++ shell/init.sh
@@ -8,7 +8,5 @@ if [ -f "$HOME/.zim/zimfw.zsh" ]; then
     zsh ~/.zim/zimfw.zsh install
     rm $HOME/.zim/zimfw.zsh
     
-    cat $HOME/.commands-ship/dependencies/brew | xargs -I _ brew install _
-    cat $HOME/.commands-ship/dependencies/python | xargs -I _ pip3 install _
     cat $HOME/.commands-ship/dependencies/snap | xargs -I _ sh -c "sudo snap install _"
 fi
