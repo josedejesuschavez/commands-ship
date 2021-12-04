f095a35f7c1109e4893a98ce95338dda29f998c2
Jose de Jesus Chavez <chavezjal06@hotmail.com>
2021-12-03 20:45:35 -0600

    Added init default for ohmyzsh

--- shell/init.sh
+++ shell/init.sh
@@ -1,13 +1,3 @@
-# This is a useful file to have the same aliases/functions in bash and zsh
-
 source "$DOTFILES_PATH/shell/aliases.sh"
 source "$DOTFILES_PATH/shell/exports.sh"
 source "$DOTFILES_PATH/shell/functions.sh"
-
-if [ -f "$HOME/.zim/zimfw.zsh" ]; then
-    zsh ~/.zim/zimfw.zsh install
-    rm $HOME/.zim/zimfw.zsh
-    
-    sudo apt install -y snapd
-    cat $HOME/.commands-ship/dependencies/snap | xargs -I _ sh -c "sudo snap install _"
-fi
