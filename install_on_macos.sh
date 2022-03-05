83b112080b5f2c94c54a8ba558ae38ff82616ebf
José Chávez <chavezjal06@hotmail.com>
2022-03-05 12:57:47 -0600

    Update install_on_macos.sh

--- install_on_macos.sh
+++ install_on_macos.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-install_brew=$(find /home -type f | grep install_brew.sh)
-install_dependencies=$(find /home -type f | grep install_dependencies.sh)
-install_oh_my_zsh=$(find /home -type f | grep install_oh-my-zsh.sh)
+current_directory=$(pwd)
+
+install_brew=$(find ${current_directory} -type f | grep install_brew.sh)
+install_dependencies=$(find ${current_directory} -type f | grep install_dependencies.sh)
+install_oh_my_zsh=$(find ${current_directory} -type f | grep install_oh-my-zsh.sh)
 
 . ${install_brew}
 . ${install_dependencies}
