3b081c5705c8e4ad5f0fe4d2e29141be2a92ad05
José Chávez <chavezjal06@hotmail.com>
2022-03-05 13:25:41 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
-echo "Hola"
-install_brew=$(find /home -type f | grep install_brew.sh)
+cd
 
-#source "$DOTFILES_PATH/shell/init.sh"
-. /home/dev/.commands-ship/install/install_brew.sh
+current_directory=$(pwd)
+current_directory="${current_directory}/.commands-ship"
+
+install_brew=$(find ${current_directory} -type f | grep install_brew.sh)
+install_dependencies=$(find ${current_directory} -type f | grep install_dependencies.sh)
+install_oh_my_zsh=$(find ${current_directory} -type f | grep install_oh-my-zsh.sh)
+
+. ${install_brew}
+. ${install_dependencies}
+. ${install_oh_my_zsh}
