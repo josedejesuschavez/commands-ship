cca105e57ea17861111e091df9bbc35ef0fc9ee9
José Chávez <chavezjal06@hotmail.com>
2022-03-05 12:44:28 -0600

    Update install_on_macos.sh

--- install_on_macos.sh
+++ install_on_macos.sh
@@ -1,1 +1,13 @@
-brew list -1 | xargs brew rm
+#!/bin/bash
+
+install_brew=$(find /home -type f | grep install_brew.sh)
+install_dependencies=$(find /home -type f | grep install_dependencies.sh)
+install_oh_my_zsh=$(find /home -type f | grep install_oh-my-zsh.sh)
+
+. ${install_brew}
+. ${install_dependencies}
+. ${install_oh_my_zsh}
+
+#curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+#	https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+#vim +'PlugInstall --sync' +qa
