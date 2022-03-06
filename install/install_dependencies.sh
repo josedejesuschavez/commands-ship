77e0d1f1879428a40a143f3bfac3c7276ebabe2a
José Chávez <chavezjal06@hotmail.com>
2022-03-05 22:30:13 -0600

    Update install_dependencies.sh

--- install/install_dependencies.sh
+++ install/install_dependencies.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 
-brew_path=$(find /home -type d | grep .linuxbrew/bin)
+cd
+
+os=$(uname)
+
+current_directory=$(pwd)
+current_directory="${current_directory}/.commands-ship"
+
+brew_path=$(find ${current_directory} -type d | grep .linuxbrew/bin)
 export PATH="$brew_path:$PATH"
 
 cat $HOME/.commands-ship/dependencies/brew | xargs -I _ brew install _
 cat $HOME/.commands-ship/dependencies/python | xargs -I _ pip3 install _
-#cat $HOME/.commands-ship/dependencies/snap | xargs -I _ sh -c "sudo snap install _"
+
+if [ "${os}" == "Linux" ]; then
+  cat $HOME/.commands-ship/dependencies/snap | xargs -I _ sh -c "sudo snap install _"
+fi
