6bd34fd460f3b450fef45e9d05861fd798b5a2c0
José Chávez <chavezjal06@hotmail.com>
2022-03-06 02:26:16 -0600

    Update exports.sh

--- shell/exports.sh
+++ shell/exports.sh
@@ -1,10 +1,10 @@
 os=$(uname)
 
 if [ "${os}" = "Linux" ]; then
-  export PATH=$HOME/.local/bin:$PATH
+  export PATH=${HOME}/.local/bin:$PATH
   export PATH=/home/linuxbrew/.linuxbrew/bin:$PATH
   export PATH=/home/linuxbrew/.linuxbrew/sbin:$PATH
-  export PATH=$PATH:/snap/bin
+  export PATH=${PATH}:/snap/bin
 fi
 
 export PATH=${DOTFILES_PATH}/bin:$PATH
