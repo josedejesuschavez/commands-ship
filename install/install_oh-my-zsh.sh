3ed8f8f10acd0df464ca808c67eb2e3333348c63
José Chávez <chavezjal06@hotmail.com>
2022-03-06 00:42:08 -0600

    Update install_oh-my-zsh.sh

--- install/install_oh-my-zsh.sh
+++ install/install_oh-my-zsh.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-brew_path=$(find /home -type d | grep .linuxbrew/bin)
+os=$(uname)
 
-echo "brew_path=$(find /home -type d | grep .linuxbrew/bin)" >> $HOME/.bashrc
-echo "exec ${brew_path}/zsh" >> $HOME/.bashrc
+if [ "${os}" == "Linux" ]; then
+  brew_path=$(find /home -type d | grep .linuxbrew/bin)
+
+  echo "brew_path=$(find /home -type d | grep .linuxbrew/bin)" >> $HOME/.bashrc
+  echo "exec ${brew_path}/zsh" >> $HOME/.bashrc
+fi
 
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
