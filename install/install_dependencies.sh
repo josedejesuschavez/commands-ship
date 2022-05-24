bf2ac2d15f4c64af5752966192e2c6195c6f8dbb
José Chávez <chavezjal06@hotmail.com>
2022-05-24 01:52:48 -0500

    Update install_dependencies.sh

--- install/install_dependencies.sh
+++ install/install_dependencies.sh
@@ -10,6 +10,6 @@ fi
 cat $HOME/.commands-ship/dependencies/brew | xargs -I _ brew install _
 cat $HOME/.commands-ship/dependencies/python | xargs -I _ pip3 install _
 
-if [ "${os}" = "Linux" ]; then
-  cat $HOME/.commands-ship/dependencies/snap | xargs -I _ sh -c "sudo snap install _"
-fi
+#if [ "${os}" = "Linux" ]; then
+#  cat $HOME/.commands-ship/dependencies/snap | xargs -I _ sh -c "sudo snap install _"
+#fi
