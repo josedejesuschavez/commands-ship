3f7dc4c0b8d0a7a85ee94d016b0f1d8ef69e7a6e
José Chávez <chavezjal06@hotmail.com>
2022-07-29 01:14:57 -0500

    Update install_on_termux.sh

--- install_on_termux.sh
+++ install_on_termux.sh
@@ -4,7 +4,7 @@ os=$(uname)
 
 current_directory="${HOME}/.commands-ship"
 
-cat $HOME/.commands-ship/dependencies/brew | xargs -I _ apt install _
+cat $HOME/.commands-ship/dependencies/brew | xargs -I _ apt -y install _
 cat $HOME/.commands-ship/dependencies/python | xargs -I _ pip install _
 
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
