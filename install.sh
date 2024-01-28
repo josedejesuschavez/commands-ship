a96d4cc6e6106f7aa245b919d1ced016927426e4
José Chávez <chavezjal06@hotmail.com>
2024-01-27 19:26:45 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,15 +4,6 @@ os=$(uname)
 
 current_directory="${HOME}/.commands-ship"
 
-if [ "${os}" = "Linux" ]; then
-  sudo apt update
-
-  sudo apt install -y build-essential procps curl file wget
-  sudo apt install libsquashfuse0 squashfuse fuse
-  #sudo apt install -y snapd
-  sudo chmod 755 /
-fi
-
 install_brew=$(find ${current_directory} -type f | grep install_brew.sh)
 install_dependencies=$(find ${current_directory} -type f | grep install_dependencies.sh)
 install_oh_my_zsh=$(find ${current_directory} -type f | grep install_oh-my-zsh.sh)
